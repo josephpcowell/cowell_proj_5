5bc99b7b91a792fe161b53f31b8c49c39318bccc
Joe Cowell <37605645+josephpcowell@users.noreply.github.com>
2020-11-28 15:11:41 -0500

    App is a bit more usable

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 """
 This .py is the structure for the Streamlit app associated with
-the corresponding project on predicting fraudulent credit card transactions.
+the corresponding project on generating The Beatles lyrics.
 """
 
 # Import streamlit
@@ -11,28 +11,53 @@ import pandas as pd
 import numpy as np
 import pickle
 
-from generator import gen_lyrics
+# from generator import gen_lyrics
+# import generator
+
+import os
+
+import gpt_2_simple as gpt2
+from datetime import datetime
+import tensorflow
+
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from transformers import AutoTokenizer, GPT2Tokenizer
 
 import os
 
 # Start of the app layout
-
 st.write(
     """# Song Generator: The Beatles
 """
 )
 
-phrase = st.text_input(
-    "Starting Phrase",
-    value="Here comes the sun",
-    max_chars=None,
-    key=None,
-    type="default",
+word = st.text_input(
+    "Enter some lyrics to start the song:", value="here comes the sun"
 )
 
-lyrics = gen_lyrics(phrase)
+st.write("The current phrase is", word)
 
-st.write(lyrics)
+
+if st.button("Generate"):
+
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess, checkpoint_dir="beatles_mod1", run_name="run1")
+    text = gpt2.generate(
+        sess,
+        run_name="run1",
+        length=500,
+        temperature=1.0,
+        top_k=0,
+        top_p=0.9,
+        prefix=word,
+        return_as_list=True,
+    )
+    lyrics = text[0]
+    st.write(lyrics)
+# lyrics = gen_lyrics("hi")
+else:
+    pass
 
 st.write(
     """
